fabffa6439334ae15d450d43d19a950efb144be0
shmibs <shmibs@gmail.com>
2015-06-24 01:25:49 -0700

    removed extraneous file
    yay keeping things simple

--- main.js
+++
@@ -1,6 +0,0 @@
-/* randomise background colour */
-window.addEventListener('load', function() {
-	var hue = Math.floor(Math.random()*360);
-	document.body.style.backgroundColor = 
-		"hsl(" + hue.toString(10) + ", 44%, 43%)";
-}, false);
